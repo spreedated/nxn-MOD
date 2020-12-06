ce94b98b13fe2acdf8b7cbe964188034c77abf05
SpReeD <wackermann@nexn.systems>
2020-12-06 17:52:58 +0100

    UnitTesting

---
+++ NUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad39cb3b-dfbf-4dc6-853d-30220b30fa50")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NUnitTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}

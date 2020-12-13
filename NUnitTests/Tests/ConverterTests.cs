294ae9b2ab23762ca153cedfe9da8d0856d96624
Markus Karl Wackermann <webmaster@born2skate.de>
2020-12-13 19:07:40 +0100

    Wave converter finished

--- NUnitTests/Tests/ConverterTests.cs
+++ NUnitTests/Tests/ConverterTests.cs
@@ -1,15 +1,9 @@
-﻿using NUnit.Framework;
-using System;
-using System.Threading.Tasks;
-using System.Threading;
-using System.IO;
-using System.Diagnostics;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿using ByteSizeLib;
 using neXn.MOD.Converter;
-using neXn.MOD.Properties;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace NUnitTests
 {
@@ -17,21 +11,48 @@ namespace NUnitTests
     public class ConverterTests
     {
         private readonly string testFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase).Replace(@"file:\", string.Empty), @"..\..\..\NUnitTests\TestFiles"));
+        private string testFile;
 
+        [SetUp]
+        public void SetUp()
+        {
+            this.testFile = Path.Combine(testFilePath, "testOut.wav");
+            DeleteTestFile();
+        }
         [Test]
         public void DontOverwriteFile()
         {
-            using (Wave k = new Wave() { OverwriteExisting = false, OutputFilepath = Path.Combine(testFilePath, "AVP.mod")} )
+            using (Wave k = new Wave() { OverwriteExisting = false, OutputFilepath = Path.Combine(testFilePath, "AVP.mod") })
             {
-                Assert.Throws<Exception>(()=> { k.Convert(); });
+                Assert.Throws<Exception>(() => { k.Convert(); });
             }
         }
         [Test]
         public void ConverstionTest()
         {
-            using (Wave k = new Wave() { OverwriteExisting = false, OutputFilepath = Path.Combine(testFilePath, "testOut.wav"), InputFilename= Path.Combine(testFilePath, "AVP.mod") })
+            using (Wave k = new Wave() { OverwriteExisting = false, OutputFilepath = this.testFile, InputFilename = Path.Combine(testFilePath, "AVP.mod") })
             {
                 k.Convert();
+            }
+            Assert.IsTrue(File.Exists(this.testFile));
+
+            FileInfo acc = new FileInfo(this.testFile);
+            Console.WriteLine($"Filesize of conversion is {ByteSize.FromBytes(acc.Length).MegaBytes:0.##} MB ({ByteSize.FromBytes(acc.Length).MebiBytes:0.##} MiB)");
+
+            Assert.Greater(acc.Length, 1024);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            DeleteTestFile();
+        }
+
+        private void DeleteTestFile()
+        {
+            if (File.Exists(this.testFile))
+            {
+                File.Delete(this.testFile);
             }
         }
     }
